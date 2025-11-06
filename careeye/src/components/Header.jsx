6394bebc8ff951766a7df6d2d97068f6ec86bdf1
hoho715 <andy2001@g.hongik.ac.kr>
2025-11-06 16:31:36 +0900

    style: 로고 삽입 및 헤더 레이아웃 및 로고 상하 여백 수정

--- careeye/src/components/Header.jsx
+++ careeye/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       {/* 로고 */}
       <div className="menubar-logo">
         <Link to="/">
-          <img src="/logo.png" alt="CareEye logo" />
+          <img src="/careeye_logo_v.png" alt="CareEye logo" />
         </Link>
       </div>
 
@@ -17,7 +17,6 @@ const Header = () => {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/monitoring">Monitoring</Link></li>
         <li><Link to="/system">System</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
