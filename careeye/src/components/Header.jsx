1e0c8268518d525aa0225957ba3528499620f173
hoho715 <andy2001@g.hongik.ac.kr>
2025-11-07 09:21:56 +0900

    Style: 로그인 버튼 hover 시 가시성 및 발광 효과 개선

--- careeye/src/components/Header.jsx
+++ careeye/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
-import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <header className="menubar-container">
@@ -16,36 +16,56 @@ const Header = () => {
       </div>
 
       {/* 중앙 메뉴 */}
-      <nav className="menubar-center">
+      <nav className={`menubar-center ${menuOpen ? "open" : ""}`}>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/monitoring">Monitoring</Link></li>
-          <li><Link to="/system">System</Link></li>
-          <li><Link to="/more">More</Link></li>
+          <li>
+            <Link to="/" onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/my-senior" onClick={() => setMenuOpen(false)}>
+              My Senior
+            </Link>
+          </li>
+          <li>
+            <Link to="/monitoring" onClick={() => setMenuOpen(false)}>
+              Monitoring
+            </Link>
+          </li>
+          <li>
+            <Link to="/notice" onClick={() => setMenuOpen(false)}>
+              Notice
+            </Link>
+          </li>
+          <li>
+            <Link to="/faq" onClick={() => setMenuOpen(false)}>
+              FAQ
+            </Link>
+          </li>
+          <li className="mobile-login">
+            <Link
+              to="/login"
+              className="login-button"
+              onClick={() => setMenuOpen(false)}
+            >
+              LOGIN
+            </Link>
+          </li>
         </ul>
       </nav>
 
       {/* 오른쪽 로그인 버튼 */}
       <div className="menubar-right">
-        <Link to="/login" className="header-login-button">
+        <Link to="/login" className="login-button">
           LOGIN
         </Link>
-        <div className="hamburger" onClick={() => setMenuOpen(true)}>
-          <FaBars />
+
+        {/* 햄버거 버튼 */}
+        <div className="hamburger" onClick={toggleMenu}>
+          {menuOpen ? "✕" : "☰"}
         </div>
       </div>
-
-      {/* 모바일 메뉴 */}
-      {menuOpen && (
-        <div className="mobile-menu">
-          <FaTimes className="close-btn" onClick={() => setMenuOpen(false)} />
-          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
-          <Link to="/monitoring" onClick={() => setMenuOpen(false)}>Monitoring</Link>
-          <Link to="/system" onClick={() => setMenuOpen(false)}>System</Link>
-          <Link to="/more" onClick={() => setMenuOpen(false)}>More</Link>
-          <Link to="/login" onClick={() => setMenuOpen(false)}>LOGIN</Link>
-        </div>
-      )}
     </header>
   );
 };
